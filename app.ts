6c8f42770a53586dd5b013419d00b4c1b45d27b4
BABARSHAH786 <159919873+BABARSHAH786@users.noreply.github.com>
2024-03-17 01:48:04 +0500

    EXERCISE 16

---
+++ app.ts
@@ -0,0 +1,26 @@
+// let languages : string [] = ['english','urdu','hindi','french','arabic']
+// console.log("list of languages:")
+// for(let top of languages){
+//     console.log(top)
+// }
+
+// Define an interface for the object structure
+interface Item {
+    name: string;
+    price: number;
+  }
+  
+  // Create two objects using the interface
+  const book: Item = {
+    name: "ESSENTIAL TYPESCRIPT ",
+    price: 12.99,
+  };
+  
+  const apple: Item = {
+    name: "Apple",
+    price: 0.99,
+  };
+  
+  // Access and print the object properties
+  console.log(`Book name: ${book.name}, price: $${book.price}`);
+  console.log(`Apple name: ${apple.name}, price: $${apple.price}`);
